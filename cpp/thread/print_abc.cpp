2ecdfb45335d6091d5efbb16574fd31fbce94003
f20500909 <3213377511@qq.com>
2020-02-14 12:38:50 +0800

    优化目录结构  增加红黑树 (copy过来的)

---
+++ cpp/thread/print_abc.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <functional>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+std::mutex mut;
+
+int cnt = 0;
+
+void func(char ch, int num) {
+    while (num--) {
+        while (ch != cnt + 'A') {
+            std::this_thread::yield();
+        }
+        unique_lock<mutex> lk(mut);
+        cout << ch;
+        cnt = (cnt + 1) % 3;
+        lk.unlock();
+    }
+};
+
+int main() {
+
+    thread t1(func, 'A', 10);
+    thread t2(func, 'B', 10);
+
+    func('C', 10);
+
+    t1.join();
+    t2.join();
+
+
+    return 0;
+}
